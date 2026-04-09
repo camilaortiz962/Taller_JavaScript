129b27e6c8df2613ad212597a9bb13ca0a85c92b
camilaortiz962 <mortizh@unbosque.edu.co>
2026-04-09 08:33:24 -0500

    commit const riesgo

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const SMLV = 1750905;
 const SMIV = 22761765;
 const SubTransporte = 249095;
 const undValorTributario = 52.37;
+const riesgo1 = 0.522;
+const riesgo2 = 1.044;
+const riesgo3 = 2.436;
+const riesgo4 = 4.350;
+const riesgo5 = 6.960;
+
+
+
